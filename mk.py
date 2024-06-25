87740073f4f8c11d63b35bcd6a5512ebd1b562ea
yousef12102008 <moh552vbnm@gmail.com>
2024-06-26 01:03:00 +0300

    Update mk.py

--- mk.py
+++ mk.py
@@ -1,4 +1,4 @@
-import requests,re
+import requests, re, base64, random, string, user_agent, time
 def Tele(ccx):
 	import requests
 	ccx=ccx.strip()
@@ -11,9 +11,12 @@ def Tele(ccx):
 	r = requests.session()
 	
 
+	
 
-	
-	
+
+
+
+
 
 
 
@@ -28,7 +31,7 @@ def Tele(ccx):
     'authority': 'payments.braintree-api.com',
     'accept': '*/*',
     'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
-    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MTkxMjMyNDIsImp0aSI6IjIzMzRmZDQ3LWI0OTgtNGQ0YS04NjU0LTdlNWI3MmJlOWQ2ZSIsInN1YiI6Ind5cDM1M2NxM3JwOWZtbmMiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6Ind5cDM1M2NxM3JwOWZtbmMiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6e319.jpUOmD_ZAzI3UP4VelEVJfPh0Es1vxKYt0SuK5lHcRR9dVJK958Z3bL4lP6YxWlkJlwxfjC3Im0PdJtO7YcNqg',
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MTk0Mzc2ODQsImp0aSI6IjY5ZjI5NDQ0LWIwNmUtNGQxZi1iNjcyLTk1MmMzZjc4NDg3OCIsInN1YiI6IjR3ZHhuYm4zcm5ueWs2dHEiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6IjR3ZHhuYm4zcm5ueWs2dHEiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0Il0sIm9wdGlvbnMiOnt9fQ.fMyBYYtUUSOhzKUrsUJa9APeNpXiwuEag4-7Yc1Ke-5hJC5nalbxX6YY0WBlUSgHvA8smhrs4TBM-r4UdDcoqw',
     'braintree-version': '2018-05-10',
     'cache-control': 'no-cache',
     'content-type': 'application/json',
@@ -48,7 +51,7 @@ def Tele(ccx):
     'clientSdkMetadata': {
         'source': 'client',
         'integration': 'custom',
-        'sessionId': 'b1fb4c50-bc9e-4fdf-9929-813c525ede8d',
+        'sessionId': 'eef2ce6b-4125-4119-9dbd-0098c3cf4776',
     },
     'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
     'variables': {
@@ -57,7 +60,7 @@ def Tele(ccx):
                 'number': n,
                 'expirationMonth': mm,
                 'expirationYear': yy,
-                'cvv': cvc,
+                'cvv':cvc,
             },
             'options': {
                 'validate': False,
@@ -71,10 +74,8 @@ def Tele(ccx):
 
 # Note: json_data will not be serialized by requests
 # exactly as it was in the original request.
-#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"b1fb4c50-bc9e-4fdf-9929-813c525ede8d"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5115581818109910","expirationMonth":"06","expirationYear":"2025","cvv":"291"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
+#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"eef2ce6b-4125-4119-9dbd-0098c3cf4776"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"5115581818109910","expirationMonth":"06","expirationYear":"2025","cvv":"291"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
 #response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
-
-
 	tok=(response.json()['data']['tokenizeCreditCard']['token'])
 	
 	
@@ -93,69 +94,70 @@ def Tele(ccx):
 
 
 
-
-
-
-
-
-
 	cookies = {
-    'cookieyes-consent': 'consentid:ejJWOUJsU3ZZQUdnaTJmWERjUzJvYVljdzFjenYyaTI,consent:no,action:yes,necessary:yes,functional:no,analytics:no,performance:no,advertisement:no,other:no',
-    'intercom-id-pl9e89b4': 'da724470-2384-453e-8fd9-ffbdb07b1928',
-    'intercom-device-id-pl9e89b4': 'c3022dbc-ff08-42f0-a308-279ef4b027ec',
-    'wordpress_logged_in_82a05395de84089a447a0878739347cd': 'bjjjcjjdj%7C1719561953%7C2i4djexwJyMUT8akoKsr7PcuxyDvYRfVClbfTgtNM1w%7C4dbeb84a9b3a4f1392ad29971578c4682909239e57e8ba08983a21c7bdcea7e4',
-    'cf_clearance': 'rf4ZTXR3b_nwU.S9do5FuPKzw.7d1.jNxHE4k9Z7ejw-1719036565-1.0.1.1-7lHl8SZgm.CkJAcOho0RHAc7T9spmNv1jpcCRm5VdVXyL6Q6Xy4LV5H7Lar_W5RTvlbcBT8UAij3By4eK.QPzw',
-    'intercom-session-pl9e89b4': 'L3NaTmJWdGUrSkNwTkJDb20rZ2pRbTV6aW9IWUJKU0pkeUpYdG8wNmg3T3oyVzZKeFdPUWRENmhlZkk1WTd5Vi0tMjZPUDkvZi90OTQ5NDFBYk1vaUt4Zz09--9bf9c037d70b74d586361b8cb48aafc22fe11af9',
+    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29',
+    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
+    'cookielawinfo-checkbox-necessary': 'yes',
+    '_gcl_au': '1.1.1962690505.1719349662',
+    '_ga': 'GA1.1.1227635990.1719349663',
+    '_fbp': 'fb.1.1719349663689.89528477480537170',
+    'cookielawinfo-checkbox-functional': 'yes',
+    'cookielawinfo-checkbox-performance': 'yes',
+    'cookielawinfo-checkbox-analytics': 'yes',
+    'cookielawinfo-checkbox-advertisement': 'yes',
+    'cookielawinfo-checkbox-others': 'yes',
+    'viewed_cookie_policy': 'yes',
+    'cli_user_preference': 'en-cli-yes-checkbox-necessary-yes-checkbox-functional-yes-checkbox-performance-yes-checkbox-analytics-yes-checkbox-advertisement-yes-checkbox-others-yes',
+    'CookieLawInfoConsent': 'eyJ2ZXIiOiIxIiwibmVjZXNzYXJ5IjoidHJ1ZSIsImZ1bmN0aW9uYWwiOiJ0cnVlIiwicGVyZm9ybWFuY2UiOiJ0cnVlIiwiYW5hbHl0aWNzIjoidHJ1ZSIsImFkdmVydGlzZW1lbnQiOiJ0cnVlIiwib3RoZXJzIjoidHJ1ZSJ9',
+    'newpass_announce': 'true',
+    'closed_announcement': 'true',
+    'wordpress_sec_22d584ae58f64e78cb2ffa7e67fadab7': 'moh5h52vbnm%7C1720559322%7CA4vP8a3LhhNoZvMlqX38sffYy4YIXJyGiYmEtvbUjYi%7C4d7f24b0256511f50f7b1ef82e6824a163f84c981c8e45f50ed5721bd13a6d8c',
+    'wordpress_logged_in_22d584ae58f64e78cb2ffa7e67fadab7': 'moh5h52vbnm%7C1720559322%7CA4vP8a3LhhNoZvMlqX38sffYy4YIXJyGiYmEtvbUjYi%7C139a76ae7f8579dd10fc23d569aa3e9159e0949b727eff66e34ec53c7c16f3db',
+    'br_lgv_stat': 'default%7Cdefault',
+    'sbjs_migrations': '1418474375998%3D1',
+    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29',
+    'sbjs_session': 'pgs%3D18%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fce4less.com%2Fmy-account%2Fadd-payment-method%2F',
+    '_ga_JVCGZDD7ML': 'GS1.1.1719349662.1.1.1719351284.24.0.1595932677',
+    '_uetsid': 'f6554730333611ef8399318838359d69',
+    '_uetvid': 'f6576db0333611ef84c20f801562952c',
 }
 
 	headers = {
-    'authority': 'www.naturaw.co.uk',
+    'authority': 'ce4less.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
     'cache-control': 'no-cache',
     'content-type': 'application/x-www-form-urlencoded',
-    # 'cookie': 'cookieyes-consent=consentid:ejJWOUJsU3ZZQUdnaTJmWERjUzJvYVljdzFjenYyaTI,consent:no,action:yes,necessary:yes,functional:no,analytics:no,performance:no,advertisement:no,other:no; intercom-id-pl9e89b4=da724470-2384-453e-8fd9-ffbdb07b1928; intercom-device-id-pl9e89b4=c3022dbc-ff08-42f0-a308-279ef4b027ec; wordpress_logged_in_82a05395de84089a447a0878739347cd=bjjjcjjdj%7C1719561953%7C2i4djexwJyMUT8akoKsr7PcuxyDvYRfVClbfTgtNM1w%7C4dbeb84a9b3a4f1392ad29971578c4682909239e57e8ba08983a21c7bdcea7e4; cf_clearance=rf4ZTXR3b_nwU.S9do5FuPKzw.7d1.jNxHE4k9Z7ejw-1719036565-1.0.1.1-7lHl8SZgm.CkJAcOho0RHAc7T9spmNv1jpcCRm5VdVXyL6Q6Xy4LV5H7Lar_W5RTvlbcBT8UAij3By4eK.QPzw; intercom-session-pl9e89b4=L3NaTmJWdGUrSkNwTkJDb20rZ2pRbTV6aW9IWUJKU0pkeUpYdG8wNmg3T3oyVzZKeFdPUWRENmhlZkk1WTd5Vi0tMjZPUDkvZi90OTQ5NDFBYk1vaUt4Zz09--9bf9c037d70b74d586361b8cb48aafc22fe11af9',
-    'origin': 'https://www.naturaw.co.uk',
+    # 'cookie': 'sbjs_current=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29; sbjs_udata=vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36; cookielawinfo-checkbox-necessary=yes; _gcl_au=1.1.1962690505.1719349662; _ga=GA1.1.1227635990.1719349663; _fbp=fb.1.1719349663689.89528477480537170; cookielawinfo-checkbox-functional=yes; cookielawinfo-checkbox-performance=yes; cookielawinfo-checkbox-analytics=yes; cookielawinfo-checkbox-advertisement=yes; cookielawinfo-checkbox-others=yes; viewed_cookie_policy=yes; cli_user_preference=en-cli-yes-checkbox-necessary-yes-checkbox-functional-yes-checkbox-performance-yes-checkbox-analytics-yes-checkbox-advertisement-yes-checkbox-others-yes; CookieLawInfoConsent=eyJ2ZXIiOiIxIiwibmVjZXNzYXJ5IjoidHJ1ZSIsImZ1bmN0aW9uYWwiOiJ0cnVlIiwicGVyZm9ybWFuY2UiOiJ0cnVlIiwiYW5hbHl0aWNzIjoidHJ1ZSIsImFkdmVydGlzZW1lbnQiOiJ0cnVlIiwib3RoZXJzIjoidHJ1ZSJ9; newpass_announce=true; closed_announcement=true; wordpress_sec_22d584ae58f64e78cb2ffa7e67fadab7=moh5h52vbnm%7C1720559322%7CA4vP8a3LhhNoZvMlqX38sffYy4YIXJyGiYmEtvbUjYi%7C4d7f24b0256511f50f7b1ef82e6824a163f84c981c8e45f50ed5721bd13a6d8c; wordpress_logged_in_22d584ae58f64e78cb2ffa7e67fadab7=moh5h52vbnm%7C1720559322%7CA4vP8a3LhhNoZvMlqX38sffYy4YIXJyGiYmEtvbUjYi%7C139a76ae7f8579dd10fc23d569aa3e9159e0949b727eff66e34ec53c7c16f3db; br_lgv_stat=default%7Cdefault; sbjs_migrations=1418474375998%3D1; sbjs_first=typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29; sbjs_session=pgs%3D18%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fce4less.com%2Fmy-account%2Fadd-payment-method%2F; _ga_JVCGZDD7ML=GS1.1.1719349662.1.1.1719351284.24.0.1595932677; _uetsid=f6554730333611ef8399318838359d69; _uetvid=f6576db0333611ef84c20f801562952c',
+    'origin': 'https://ce4less.com',
     'pragma': 'no-cache',
-    'referer': 'https://www.naturaw.co.uk/my-account/add-payment-method/',
+    'referer': 'https://ce4less.com/my-account/add-payment-method/',
     'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-    'sec-ch-ua-arch': '""',
-    'sec-ch-ua-bitness': '""',
-    'sec-ch-ua-full-version': '"124.0.6327.4"',
-    'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.4"',
     'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-model': '"23053RN02A"',
     'sec-ch-ua-platform': '"Android"',
-    'sec-ch-ua-platform-version': '"14.0.0"',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
     'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
 }
 
-	data = [
-    ('payment_method', 'braintree_credit_card'),
-    ('wc-braintree-credit-card-card-type', 'master-card'),
-    ('wc-braintree-credit-card-3d-secure-enabled', ''),
-    ('wc-braintree-credit-card-3d-secure-verified', ''),
-    ('wc-braintree-credit-card-3d-secure-order-total', '0.00'),
-    ('wc_braintree_credit_card_payment_nonce', tok,),
-    ('wc_braintree_device_data', '{"correlation_id":"f859f0484f9fb8c81ed513b3765126b8"}'),
-    ('wc-braintree-credit-card-tokenize-payment-method', 'true'),
-    ('wc_braintree_paypal_payment_nonce', ''),
-    ('wc_braintree_device_data', '{"correlation_id":"f859f0484f9fb8c81ed513b3765126b8"}'),
-    ('wc-braintree-paypal-context', 'shortcode'),
-    ('wc_braintree_paypal_amount', '0.00'),
-    ('wc_braintree_paypal_currency', 'GBP'),
-    ('wc_braintree_paypal_locale', 'en_gb'),
-    ('wc-braintree-paypal-tokenize-payment-method', 'true'),
-    ('woocommerce-add-payment-method-nonce', '8aaa863334'),
-    ('_wp_http_referer', '/my-account/add-payment-method/'),
-    ('woocommerce_add_payment_method', '1'),
-]
+	data = {
+    'payment_method': 'braintree_credit_card',
+    'wc-braintree-credit-card-card-type': 'master-card',
+    'wc-braintree-credit-card-3d-secure-enabled': '',
+    'wc-braintree-credit-card-3d-secure-verified': '',
+    'wc-braintree-credit-card-3d-secure-order-total': '0.00',
+    'wc_braintree_credit_card_payment_nonce': tok,
+    'wc_braintree_device_data': '',
+    'wc-braintree-credit-card-tokenize-payment-method': 'true',
+    'woocommerce-add-payment-method-nonce': '9cbb1acf3a',
+    '_wp_http_referer': '/my-account/add-payment-method/',
+    'woocommerce_add_payment_method': '1',
+}
 
-	response = requests.post('https://www.naturaw.co.uk/my-account/add-payment-method/', cookies=cookies, headers=headers, data=data)
+	response = requests.post('https://ce4less.com/my-account/add-payment-method/', cookies=cookies, headers=headers, data=data)
 	text=(response.text)
 	import re
 	pattern = r"Status code \d+: (.+?)\s*</li>"
@@ -172,4 +174,5 @@ def Tele(ccx):
 		else:
 			print(text)
 			return 'risk_threshold'
+			
 	
